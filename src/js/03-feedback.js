eb4bee8594db8d34d8e5299bf5d7a6280de191a4
MadDog83 <130376768+MadDog83@users.noreply.github.com>
2023-10-03 22:57:05 +0200

    add\feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+let formData = {};
+
+// Завантажує дані з локального сховища і заповнює поля форми
+function populateFormFields() {
+  const savedFormData = localStorage.getItem(STORAGE_KEY);
+  if (savedFormData) {
+    formData = JSON.parse(savedFormData);
+    for (const [name, value] of Object.entries(formData)) {
+      form.elements[name].value = value;
+    }
+  }
+}
+
+// Зберігає дані форми в локальне сховище
+const saveFormData = e => {
+  formData = {...formData, [e.target.name]: e.target.value};
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+// Очищує дані форми і локальне сховище
+const clearFormData = () => {
+  form.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+// Викликає функцію populateFormFields при завантаженні сторінки
+populateFormFields();
+
+// Додає обробники подій для введення і відправлення форми
+form.addEventListener('input', throttle(saveFormData, 500));
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  console.log(formData);
+  clearFormData();
+});
